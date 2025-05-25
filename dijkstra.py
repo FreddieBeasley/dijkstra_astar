323d7e4868a37a04c4776b911ca4bb2084973739
Freddie Beasley <frederickbeasley1210+github@gmail.com>
2025-05-25 13:37:50 +0100

    🛁 cleaned up code
    removed unused methods and functions
    removed unnecessary comments

--- dijkstra.py
+++ dijkstra.py
@@ -1,20 +1,6 @@
 from graph import *
 
 #Not used in the end
-"""
-def getAdjacentVertices(graph:Graph, vertex:Vertex) -> list:
-     returnlist = []
-
-     for edge in graph.get_edges():
-          
-          if vertex == edge.get_vertices()[0] and edge.get_vertices()[1] not in returnlist:
-               returnlist.append(edge.get_vertices()[1])
-
-          elif vertex == edge.get_vertices()[1] and edge.get_vertices()[0] not in returnlist:
-               returnlist.append(edge.get_vertices()[0])
-     
-     return returnlist
-"""
 
 class DijkstraVertex(Vertex):
      def __init__(self, lable, order, count, path=None):
@@ -31,11 +17,6 @@ class DijkstraVertex(Vertex):
      
      def get_path(self):
           return self.__path
-     
-     def get_path_reverse(self):
-          #logic means that path is stored in reverse
-          return self.__path
-
      
      def update_order(self, order):
           self.__order = order
@@ -154,39 +135,11 @@ def dijkstra(graph:Graph, startVertex:Vertex, endVertex:Vertex = None) -> list:
                     queue.add(discoveredDijkstraVertex)
                     discoveredDijkstraVertex.update_path(currentDijkstraItem.get_path() + discoveredDijkstraVertex.get_lable())
                
-          '''
-          for edge in graph.get_edges():
-               #loops through each vertex in the graph
-
-               if edge.get_vertices()[0] == graph.lookUp_vertex(currentDijkstraItem.get_lable()):
-                    #selects vertices which are adjacent to current vertex
-                    consideringDijkstraItem = queue.lookUp_item(edge.get_vertices()[1].get_lable())
-
-               elif edge.get_vertices()[1] == graph.lookUp_vertex(currentDijkstraItem.get_lable()):
-                    consideringDijkstraItem = queue.lookUp_item(edge.get_vertices()[0].get_lable())
-
-               else:
-                    continue
-
-               if consideringDijkstraItem == None:
-                    continue
-               
-               currentRouteCost = consideringDijkstraItem.get_count()
-               newRouteCost = currentDijkstraItem.get_count() + edge.get_weight()
-
-               if currentRouteCost > newRouteCost:
-                    consideringDijkstraItem.update_count(newRouteCost)
-                    consideringDijkstraItem.update_path(currentDijkstraItem.get_path())
-               '''
      if specific:
           endVertexDijkstra = queue.lookUp_dijkstraVertex_resolved(endVertex.get_lable())
-          return [f"vertex: {endVertexDijkstra.get_lable()}", f"order: {endVertexDijkstra.get_order()}", f"cost: {endVertexDijkstra.get_count()}", f"path: {endVertexDijkstra.get_path_reverse()}"]
+          return [f"vertex: {endVertexDijkstra.get_lable()}", f"order: {endVertexDijkstra.get_order()}", f"cost: {endVertexDijkstra.get_count()}", f"path: {endVertexDijkstra.get_path()}"]
      
-     return [[f"vertex: {item.get_lable()}", f"order: {item.get_order()}", f"cost: {item.get_count()}", f"path: {item.get_path_reverse()}"] for item in queue.get_resolved()]
-
-'''
-if you pass in a vertex that it not in the graph as the end vertex (e.g. linear graph test 1 where there is no vertex g), due to the lookUp_vertex returning None when the vertex cannot be found, None is passed in as the endVertex and therefore the dijkstra algorithm will just find the shortest path to all other nodes.
-'''
+     return [[f"vertex: {item.get_lable()}", f"order: {item.get_order()}", f"cost: {item.get_count()}", f"path: {item.get_path()}"] for item in queue.get_resolved()]
 
 # --- Test 1: my_graph ---
 print("\n--- Test 1: my_graph ---")
